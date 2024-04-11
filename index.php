53436fc53515cb97c5944e6ca514cc0671d4de93
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 10:07:37 +0200

    modification de index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$page_description = "description de la page d'accueil";
+$page_title = "titre de la page d'accueil";
+$page_content = "<h1> hello world </h1>";
+require_once("view/layouts/base.php");*
+?>
