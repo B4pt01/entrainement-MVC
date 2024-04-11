6aae7de00c4a1ec497692fdf785a39d26aedb1f8
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 10:41:06 +0200

    menu.php

---
+++ view/layouts/menu.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="container">
+        <button id="menuButton" type="button">
+            <svg fill="none" stroke="currentColor" viewBox="8 8 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16H4 12h16m-7 6h7"></path>
+            </svg>
+        </button>
+
+        <div class="hidden" id="mobileMenu">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Page1</a></li>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
